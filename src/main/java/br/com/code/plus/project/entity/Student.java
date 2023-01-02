99f63b47fade3861fade98697f916c867bc60516
unknown <ruchellguilherme100@gmail.com>
2023-01-02 19:23:47 -0300

    retirando as anotacoes getter e setter e gerando com a ide

--- src/main/java/br/com/code/plus/project/entity/Student.java
+++ src/main/java/br/com/code/plus/project/entity/Student.java
@@ -7,8 +7,6 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -30,4 +28,51 @@ public class Student {
     @Column(name = "telefone", length = 15)
     private String phone;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
