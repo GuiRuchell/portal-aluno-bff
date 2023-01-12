71cff830a16d9f673de7c3df703d87c7ff599be8
unknown <ruchellguilherme100@gmail.com>
2023-01-11 22:31:45 -0300

    gerando getters e setters utilizando lombok

--- src/main/java/br/com/code/plus/project/entity/Student.java
+++ src/main/java/br/com/code/plus/project/entity/Student.java
@@ -9,6 +9,8 @@ import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name= "aluno")
 public class Student {
@@ -28,51 +30,4 @@ public class Student {
     @Column(name = "telefone", length = 15)
     private String phone;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
